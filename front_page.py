02a1720e5db2833bf82d7a5b25709fab23978722
Khubaib Lodhi <lodhikhubaib12@gmail.com>
2023-11-20 16:09:02 +0500

    Add a messagebox at the time of cancel option

--- front_page.py
+++ front_page.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 import importlib
+from tkinter import Tk, Canvas, Button, BOTTOM, StringVar, OptionMenu, Label
+from tkinter import Canvas, messagebox
 
 def start_algorithm(root):
     root.destroy()
@@ -11,9 +13,21 @@ def start_algorithm(root):
 root = tk.Tk()
 root.title("Algorithm Project")
 
+# Set the background color to white
+root.configure(background="white")
+
+
+# Get screen width and height
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+# Calculate the new width and height for the image (100% of the screen width and height)
+image_width = int(0.7 * screen_width)
+image_height = int(0.7 * screen_height)
+
 # Load and display an image on the front page
-image = Image.open(r"C:\Users\HP\OneDrive\Desktop\Geometric_Algorithms\Geometric_Algorithm\src\images\Back.jpeg")  # Replace with your image file
-image = image.resize((700, 500), Image.LANCZOS)
+image = Image.open(r"C:\Users\HP\OneDrive\Desktop\algo\Line inte\Tkinter\AlgoProject\Back.jpeg")  # Replace with your image file
+image = image.resize((image_width, image_height), Image.LANCZOS)
 photo = ImageTk.PhotoImage(image)
 
 # Create a label to display the image
@@ -22,11 +36,19 @@ image_label.image = photo
 image_label.pack()
 
 # Add a title label
-title_label = tk.Label(root, text="Welcome to Algorithm Project", font=("Helvetica", 20, "bold"))
+title_label = tk.Label(root, text="Welcome to Algorithm Project", font=("Helvetica", 20, "bold"), background="white")
 title_label.pack(pady=20)
 
 # Add a start button
 start_button = tk.Button(root, text="Start", font=("Helvetica", 12, "bold"), command=lambda: start_algorithm(root))
-start_button.pack(pady=2)
+start_button.pack(pady=10)
 
+def on_close():
+        # Prompt the user with a Yes/No messagebox
+        user_response = messagebox.askyesno("Confirmation", "Do you want to close the application?")
+        if user_response:
+            print("Tkinter application closed.")
+            root.destroy()
+
+root.protocol("WM_DELETE_WINDOW", on_close)
 root.mainloop()
